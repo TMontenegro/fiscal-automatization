ec099329feee6afa3dd6393a33ee1ee87bc8e0a1
Tales Dias <montenegro.tales@gmail.com>
2020-06-23 00:57:49 -0300

    Initial project with angular and angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticateComponent } from './views/authenticate/authenticate.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: AuthenticateComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
