37f653cefb7f0a8f8ffa6160ffa72e8125e04e91
Tales Dias <montenegro.tales@gmail.com>
2020-06-29 22:14:43 -0300

    Create register and forgot-password components and link authenticate to them; add logo image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticateComponent } from './views/authenticate/authenticate.component';
+import { RegisterComponent } from './views/register/register.component';
+import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: AuthenticateComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RegisterComponent } from './views/register/register.component';
+import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
 @NgModule({
-  declarations: [AppComponent, AuthenticateComponent, DashboardComponent],
+  declarations: [AppComponent, AuthenticateComponent, DashboardComponent, RegisterComponent, ForgotPasswordComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
